0d040a14985b33cb2e563a1fc1c3f3edd7682b5e
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-15 18:00:40 +0100

    hotovo

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -4,9 +4,19 @@ namespace Open_Lab_04._02
 {
     public class Doubler
     {
-        public string DoubleChar(string original)
+        public string DoubleChar(string original_string)
         {
-            throw new NotImplementedException();
+            int position = 0;
+            char[] characters = original_string.ToCharArray();
+            char[] New_Characters = new char[characters.Length * 2];
+            foreach (char character in characters)
+            {
+                New_Characters[position] = character;
+                New_Characters[position + 1] = character;
+
+                position += 2;
+            }
+            return new string(New_Characters);
         }
     }
 }
